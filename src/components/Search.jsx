95566661f734f956bc11497a0cdebe23584f492e
zz707 <zarinmbox@gmail.com>
2022-03-16 14:20:01 +0300

    Rewrite to functional components and hooks

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,37 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-class Search extends React.Component {
-    state = {
-        search: '',
-        type: 'all',
-    }
+const Search = (props) => {
+    const {
+        searchMovies = Function.prototype,
+    } = props;
 
-    handleKey = (event) => {
+    const [search, setSearch] = useState('');
+    const [type, setType] = useState('all');
+
+    const handleKey = (event) => {
         if (event.key === 'Enter') {
-            this.props.searchMovies(this.state.search, this.state.type);
+            searchMovies(search, type);
         }
     }
 
-    handleFilter = (event) => {
-        this.setState(() => ({type: event.target.dataset.type}), () => {
-            this.props.searchMovies(this.state.search, this.state.type);
-        })
+    const handleFilter = (event) => {
+        setType(event.target.dataset.type);
+        searchMovies(search, event.target.dataset.type);
     }
 
-    render() {
+
         return (<div className="row">
             <div className="input-field">
                 <input
                     placeholder="search"
                     type="search"
                     className="validate"
-                    value={this.state.search}
-                    onChange={(e) => this.setState({search: e.target.value})}
-                    onKeyDown={this.handleKey}
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleKey}
                 />
                 <button
                     className="btn search-btn"
-                    onClick={() => this.props.searchMovies(this.state.search, this.state.type)}
+                    onClick={() => searchMovies(search, type)}
                 >Search
                 </button>
             </div>
@@ -42,8 +43,8 @@ class Search extends React.Component {
                         className="with-gap"
                         name="type"
                         data-type="all"
-                        onChange={this.handleFilter}
-                        checked={this.state.type === 'all'}
+                        onChange={handleFilter}
+                        checked={type === 'all'}
                     />
                     <span>All</span>
                 </label>
@@ -53,8 +54,8 @@ class Search extends React.Component {
                         className="with-gap"
                         name="type"
                         data-type="movie"
-                        onChange={this.handleFilter}
-                        checked={this.state.type === 'movie'}
+                        onChange={handleFilter}
+                        checked={type === 'movie'}
                     />
                     <span>Movies only</span>
                 </label>
@@ -64,14 +65,14 @@ class Search extends React.Component {
                         className="with-gap"
                         name="type"
                         data-type="series"
-                        onChange={this.handleFilter}
-                        checked={this.state.type === 'series'}
+                        onChange={handleFilter}
+                        checked={type === 'series'}
                     />
                     <span>Series only</span>
                 </label>
             </div>
         </div>)
-    }
+
 }
 
 export {Search}
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,53 +1,56 @@
 import {Movies} from "../components/Movies";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Preloader} from "../components/Preloader";
 import {Search} from "../components/Search";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends React.Component {
+const Main = () => {
 
-    state = {
-        movies: [],
-        loading: true,
-    };
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    searchMovies = (searchString, type = 'all') => {
-        this.setState({loading: true});
+    const searchMovies = (searchString, type = 'all') => {
+        setLoading(true);
 
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchString}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => {
+                setMovies(data.Search);
+                setLoading(false);
+            })
             .catch((error) => {
                 console.error(error);
-                this.setState({loading: false})
+                setLoading(false)
             });
     }
 
-    componentDidMount() {
+    useEffect(() =>
+    {
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => {
+                setMovies(data.Search);
+                setLoading(false);
+            })
             .catch((error) => {
                 console.error(error);
-                this.setState({loading: false})
+                setLoading(false)
             });
-    }
+    }, []);
 
-    render() {
-        const {movies, loading} = this.state;
 
-        return <main className="container content">
-            <Search searchMovies={this.searchMovies}/>
-            {
-                loading ? (
-                    <Preloader/>
-                ) : (
-                    <Movies movies={movies}/>
-                )
-            }
-        </main>;
-    }
+    return <main className="container content">
+        <Search searchMovies={searchMovies}/>
+        {
+            loading ? (
+                <Preloader/>
+            ) : (
+                <Movies movies={movies}/>
+            )
+        }
+    </main>;
+
 }
 
 export {Main};
